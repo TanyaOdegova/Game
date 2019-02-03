7a057197770e62db64e8f0607a68231f15f0f240
TanyaOdegova <odegovat@yahoo.com>
2019-02-03 17:01:37 -0500

    <show lives fixed>

--- assets/game.js
+++ assets/game.js
@@ -3,7 +3,11 @@ window.addEventListener = function () {
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  
+        
+        var showLives = document.getElementById("mylives");
+        var showCatagory = document.getElementById("scatagory");
+        var getHint = document.getElementById("hint");
+        var showClue = document.getElementById("clue");
   // create the list for alphabet
   var buttons = function () {
     myButtons = document.getElementById('buttons');
